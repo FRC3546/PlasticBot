1991ffd44579075418d5e243fb5d527d5585ee2b
Andrew Dickinson <fortknoxcomputer@gmail.com>
2015-01-20 19:58:02 -0500

    Implemented methods for janky cylinder

--- src/org/usfirst/frc/team3546/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team3546/robot/subsystems/Pneumatics.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3546.robot.subsystems;
 
+import org.usfirst.frc.team3546.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Solenoid;
 
@@ -7,11 +9,39 @@ import edu.wpi.first.wpilibj.Solenoid;
  * Contains the single cylinder present on plasticbot
  */
 public class Pneumatics extends Subsystem {
-    
+    public static boolean JANKYCYLINDEREXTENDED = true;
+    public static boolean JANKYCYLINDERRETRACTED = false;
     Solenoid jankyCylinder;
 
     public void initDefaultCommand() {
-        jankyCylinder = new Solenoid();
+        jankyCylinder = new Solenoid(RobotMap.jankyCylinder);
+    }
+    
+    /**
+     * Get the position of the janky cylinder
+     * @return the position of the janky cylinder
+     */
+    public boolean getJankyCylinderPos(){
+    	return jankyCylinder.get();
+    }
+    
+    /**
+     * Sets the position of the janky cylinder
+     * @param position The new position of the cylinder
+     */
+    public void setJankyCylinderPos(boolean position){
+    	jankyCylinder.set(position);
+    }
+    
+    /**
+     * Toggles the position of the janky cylinder
+     */
+    public void toggleJankyCylinder(){
+    	if (getJankyCylinderPos() == JANKYCYLINDEREXTENDED){
+    		setJankyCylinderPos(JANKYCYLINDERRETRACTED);
+    	} else if (getJankyCylinderPos() == JANKYCYLINDERRETRACTED){
+    		setJankyCylinderPos(JANKYCYLINDEREXTENDED);
+    	}
     }
 }
 
